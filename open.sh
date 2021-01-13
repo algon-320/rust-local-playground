14182c3eb56791842949cd92557afd944c6ba892
algon-320 <algon.0320@gmail.com>
2021-01-13 15:38:33 +0900

    auto git commit

--- open.sh
+++ open.sh
@@ -3,6 +3,7 @@ set -euf -o pipefail
 
 TEMPLATE="./template.rs"
 CODE_DIR="./codes"
+AUTO_GIT=1
 
 default="unamed-$(date -Iseconds)"
 name="${1:-$default}"
@@ -16,8 +17,13 @@ function discard() {
     exit 1
 }
 
+function git_add_commit() {
+    [[ ${AUTO_GIT} -eq 1 ]] && git add "$1" && git commit -m "$2"
+}
+
 if [[ -f "$(filepath)" ]]; then
-    "${EDITOR:-vi}" "$(filepath)"
+    "${EDITOR:-vi}" "$(filepath)" || true
+    git_add_commit "$(filepath)" "Update $(filepath)"
     exit 0
 fi
 
@@ -27,16 +33,18 @@ cp "$TEMPLATE" "$(filepath)"
 
 while true; do
     echo "Do you wish to save this as '$(filepath)'?"
-    read -p "[YES/no/rename] > " ans
+    read -rp "[YES/no/rename] > " ans
     a="${ans:0:1}"
     case "${a^^}" in
        '' ) ;&
         Y ) break;;
         N ) discard ;;
         R ) old_path="$(filepath)"
-            read -p "New name? (save as $(filepath [new-name])) > " name
+            read -rp "New name? (save as $(filepath '[new-name]')) > " name
             mv "$old_path" "$(filepath)" ;;
         * ) ;;
     esac
 done
+
 echo "Saved as '$(filepath)'."
+git_add_commit "$(filepath)" "Add $(filepath)"

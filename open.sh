456767320eddfa1fc1b65e0968ce5efaad448b5e
algon-320 <algon.0320@gmail.com>
2021-01-16 09:40:21 +0900

    update the script
    Add auto-amend feature

--- open.sh
+++ open.sh
@@ -3,31 +3,40 @@ set -euf -o pipefail
 
 TEMPLATE="./template.rs"
 CODE_DIR="./codes"
-AUTO_GIT=1
+DEFAULT_FILENAME="unamed-$(date -Iseconds)"
 
-default="unamed-$(date -Iseconds)"
-name="${1:-$default}"
+# 0: disable git feature
+# 1: enable git feature with independent commits
+#    (make a independent commit for each change)
+# 2: enable git feature with amendment
+#    (amend the last commit if it has changes for the same file)
+AUTO_GIT=2
+
+name="${1:-$DEFAULT_FILENAME}"
 
 function filepath() {
     echo "${CODE_DIR}/${1:-$name}.rs"
 }
 
-function discard() {
-    rm "$(filepath)"
-    exit 1
-}
+function git_update() {
+    # do nothing if the git feature is disabled
+    [[ ${AUTO_GIT} -ne 0 ]] || return
 
-function git_add_commit() {
-    [[ ${AUTO_GIT} -eq 1 ]] && git add "$1" && git commit -m "$2"
+    local file="$1" message="$2"
+    if (git log --pretty=oneline -1 | grep "$message"); then
+        git add "$file" && git commit --amend --no-edit
+    else
+        git add "$file" && git commit -m "$message"
+    fi
 }
 
 if [[ -f "$(filepath)" ]]; then
-    "${EDITOR:-vi}" "$(filepath)" || true
-    git_add_commit "$(filepath)" "Update $(filepath)"
+    "${EDITOR:-vi}" "$(filepath)"
+    git_update "$(filepath)" "Update $(filepath)"
     exit 0
 fi
 
-mkdir -p "$CODE_DIR"
+mkdir -p "$(dirname "$(filepath)")"
 cp "$TEMPLATE" "$(filepath)"
 "${EDITOR:-vi}" "$(filepath)"
 
@@ -36,15 +45,17 @@ while true; do
     read -rp "[YES/no/rename] > " ans
     a="${ans:0:1}"
     case "${a^^}" in
-       '' ) ;&
-        Y ) break;;
-        N ) discard ;;
-        R ) old_path="$(filepath)"
+        '' | Y ) break;;
+        N ) : Discard changes
+            rm "$(filepath)"
+            exit 1 ;;
+        R ) : Rename the file
+            old_path="$(filepath)"
             read -rp "New name? (save as $(filepath '[new-name]')) > " name
             mv "$old_path" "$(filepath)" ;;
-        * ) ;;
+        * ) : Retry ;;
     esac
 done
 
 echo "Saved as '$(filepath)'."
-git_add_commit "$(filepath)" "Add $(filepath)"
+git_update "$(filepath)" "Add $(filepath)"

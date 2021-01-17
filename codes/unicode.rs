df4437562fd9b444eee2b2d3ea05b459b5456d0b
algon-320 <algon.0320@gmail.com>
2021-01-17 17:36:24 +0900

    Add ./codes/unicode.rs

---
+++ codes/unicode.rs
@@ -0,0 +1,8 @@
+//---- Put dependencies above ----
+
+#![allow(dead_code, unused_variables)]
+
+fn main() {
+    println!("Wellcome to the playground!");
+    println!("ヘ\u{3099}ストアルハ\u{3099}ム");
+}

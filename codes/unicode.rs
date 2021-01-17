4dd12ea779494ff3cda8e339efc1cd0e69d9b291
algon-320 <algon.0320@gmail.com>
2021-01-17 18:19:44 +0900

    Update ./codes/unicode.rs

--- codes/unicode.rs
+++ codes/unicode.rs
@@ -1,8 +1,25 @@
+//# unicode-normalization = "0.1.8"
 //---- Put dependencies above ----
 
 #![allow(dead_code, unused_variables)]
 
 fn main() {
     println!("Wellcome to the playground!");
-    println!("ヘ\u{3099}ストアルハ\u{3099}ム");
+    let msg = "ヘ\u{3099}ストアルハ\u{3099}ム";
+    println!("{}", msg);
+    {
+        use unicode_normalization::UnicodeNormalization;
+        let msg_norm = msg.chars().nfc().collect::<String>();
+        println!("{}", msg_norm);
+    }
+
+    let s = "が";
+    println!("{:?} ==> {:?}", s, s.as_bytes());
+    let s = "か\u{3099}";
+    println!("{:?} ==> {:?}", s, s.as_bytes());
+    let s = "か";
+    println!("{:?} ==> {:?}", s, s.as_bytes());
+
+    let c = unicode_normalization::char::compose('か', '\u{3099}');
+    println!("{:?}", c);
 }

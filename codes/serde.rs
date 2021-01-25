2209a0b40c6a776be3266b18fa689bd3f7418d05
algon-320 <algon.0320@gmail.com>
2021-01-25 10:30:51 +0900

    Update ./codes/serde.rs

--- codes/serde.rs
+++ codes/serde.rs
@@ -5,25 +5,41 @@
 #![allow(dead_code, unused_variables)]
 
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Heavy(String);
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct FooRef<'a> {
-    x: &'a str,
+    x: Cow<'a, Heavy>,
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct FooOwn {
-    x: String,
+    x: Heavy,
+}
+
+use std::collections::HashSet;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Set {
+    c: HashSet<i32>,
 }
 
 fn main() {
     let h = Heavy("heavy".into());
-    let f = FooRef { x: &h.0 };
+    let f = FooRef {
+        x: Cow::Borrowed(&h),
+    };
     dbg!(serde_json::to_string(&f));
 
     let h = Heavy("heavy".into());
-    let f = FooOwn { x: h.0 };
+    let f = FooOwn { x: h };
     dbg!(serde_json::to_string(&f));
+
+    let json = r#"{"c":[1,2,3]}"#;
+    let s: Set = serde_json::from_str(json).unwrap();
+    dbg!(s);
+    let json = r#"{"c":[1,2,3,1,1,1,1]}"#;
+    let s: Set = serde_json::from_str(json).unwrap();
+    dbg!(s);
 }

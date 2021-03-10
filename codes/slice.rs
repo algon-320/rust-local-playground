fbb1cd929b50ccedd307e30a2f5977f1a894df33
algon-320 <algon.0320@gmail.com>
2021-03-10 16:01:27 +0900

    Update ./codes/slice.rs

--- codes/slice.rs
+++ codes/slice.rs
@@ -11,4 +11,20 @@ fn main() {
     assert_eq!(it.as_slice(), &s[1..]);
     it.next().unwrap();
     assert_eq!(it.as_slice(), &s[2..]);
+
+    match s {
+        [1, 2, 3, 4, 5] => {}
+        _ => unreachable!(),
+    }
+    match s[1..] {
+        [1, 2, 3, 4, 5] => unreachable!(),
+        [2, 3, 4, 5] => {}
+        _ => unreachable!(),
+    }
+    match s[2..] {
+        [1, 2, 3, 4, 5] => unreachable!(),
+        [2, 3, 4, 5] => unreachable!(),
+        [3, 4, 5] => {}
+        _ => unreachable!(),
+    }
 }
